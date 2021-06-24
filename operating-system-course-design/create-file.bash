688a7dd4f6321e46d98ff446a547eaa399d584b0
crupest <crupest@outlook.com>
2021-06-25 07:28:48 +0800

    ...

--- operating-system-course-design/create-file.bash
+++ operating-system-course-design/create-file.bash
@@ -1,13 +1,29 @@
-let file_count=2*1024*2;
 
-for iter in `seq $file_count`; do
-    truncate -s 512KB /mnt/fat-test/$iter;
+let dir_count=256
+let file_count=500;
+
+for dir_iter in `seq $dir_count`; do
+    mkdir -p /mnt/fat-test/$dir_iter
+    for iter in `seq $file_count`; do
+        truncate -s 4KB /mnt/fat-test/$dir_iter/$iter;
+    done
+    echo "Created 500 files in dir $dir_iter";
 done
 
-for iter in `seq $file_count`; do
-    if (($iter % 2 == 0)); then
-        rm /mnt/fat-test/$iter;
-    fi
+df
+
+for dir_iter in `seq $dir_count`; do
+    mkdir -p /mnt/fat-test/$dir_iter
+    for iter in `seq $file_count`; do
+        if (($iter % 2 == 0)); then
+            rm /mnt/fat-test/$dir_iter/$iter;
+        fi
+    done
+    echo "Deleted 250 files in dir $dir_iter";
 done
 
-truncate -s 500MB /mnt/fat-test/big;
+df
+
+truncate -s 200MB /mnt/fat-test/big;
+
+df
